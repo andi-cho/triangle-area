6fc59a1605fb396c4b3f1c14dddbeca5c9d3ec85
andi-cho <andrea.cho456@gmail.com>
2022-04-09 17:15:55 -0400

    add triangle file

---
+++ triangle-area.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double s1, s2, s3, p, a;
+	cout << "Enter first number: " << endl;
+	cout << "Enter second number: " << endl;
+	cout << "Enter third number: " << endl;
+	cin >> s1 >> s2 >> s3;
+	getSides(s1, s2, s3, p, a); 	
+	return 0;
+}
+
+void getSides(double s1, double s2, double s3, double p, double a) 
+{
+	printf("getSides():");
+	return;
+}
