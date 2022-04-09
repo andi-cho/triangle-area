648387994b4f55873bf790a6bb64d8f4bf899c62
andi-cho <andrea.cho456@gmail.com>
2022-04-09 18:32:01 -0400

    works

--- triangle-area.cpp
+++ triangle-area.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+void getSides(double*, double*, double*); 
+void triangle(double, double, double, double*, double*); 
 
 int main() {
 	double s1, s2, s3, p, a;
-	cout << "Enter first number: " << endl;
-	cout << "Enter second number: " << endl;
-	cout << "Enter third number: " << endl;
-	cin >> s1 >> s2 >> s3;
-	getSides(s1, s2, s3, p, a); 	
+	getSides(&s1, &s2, &s3); 
+	if (s1 <= 0 || s2 <= 0 || s3 <= 0) { return -1; }
+	triangle(s1, s2, s3, &p, &a);	
+	
+	// area must be positive and a number
+	if (isnan(a) || a <= 0) 
+	{
+		cout << s1 << "," << s2 << "," << s3 <<": not a triangle" << endl;
+		return 0; 
+	}
+
+	// output perimeter + area for valid triangle
+	cout << s1 << "," << s2 << "," << s3 << ": perimeter=" << p << " area=" << a << endl;
 	return 0;
 }
 
-void getSides(double s1, double s2, double s3, double p, double a) 
+// get 3 sides from user input
+void getSides(double* s1, double* s2, double* s3) 
 {
-	printf("getSides():");
-	return;
+	cout << "Enter first number: ";
+	cin >> *s1;
+	if (*s1 <= 0) { return; }
+
+	cout << "Enter second number: ";
+	cin >> *s2;
+	if (*s2 <= 0) { return; }
+
+	cout << "Enter third number: ";
+	cin >> *s3;
+	if (*s3 <= 0) { return; }
+}
+
+// compute area + perimeter
+void triangle(double s1, double s2, double s3, double* p, double *a)
+{
+	// perimeter  
+	*p = s1 + s2 + s3;
+
+	// semi-perimeter
+	double s = (s1 + s2 + s3) / 2;
+
+	// area 
+	*a = sqrt( s * (s-s1) * (s-s2) * (s-s3) );
 }
